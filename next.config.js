e7c38a0dfe865ec5c34253e5fe0ad4ee4ed02c24
josh-dykstra <joshuajdykstra@gmail.com>
2023-05-06 16:26:43 -0700

    fixup next js file to use module imports

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const path = require('path');
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
+module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
 };
-
-export default nextConfig;
